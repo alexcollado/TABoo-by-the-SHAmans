f5b6c76ba456185715a19e2cba93b4125f541a2d
Neil Opena <neil1114o@gmail.com>
2019-10-12 15:57:30 -0400

    Updated popup

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function (data) {
+    // changeColor.style.backgroundColor = data.color;
+    // changeColor.setAttribute('value', data.color);
+// });
 
 changeColor.onclick = function (element) {
     let color = element.target.value;
