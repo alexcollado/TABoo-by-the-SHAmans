20175a41cdc475f7ebe1266bfd0d7493374810f4
Neil Opena <neil1114o@gmail.com>
2019-11-04 08:33:52 -0500

    Basic comparison works

--- background.js
+++ background.js
@@ -45,7 +45,6 @@ function screenshotTab(tabId) {
     chrome.storage.sync.get('protection', function (data) {
         if (data.protection) {
             chrome.tabs.captureVisibleTab(function (dataURL) {
-                console.log(dataURL);
                 var oldURL = screenshotMap.get(tabId);
                 if (oldURL != undefined) {
                     compareDataURLs(oldURL, dataURL, tabId);
@@ -53,40 +52,119 @@ function screenshotTab(tabId) {
                     chrome.storage.sync.set({ color: "#dadada" }, null);
                 }
                 screenshotMap.set(tabId, dataURL);
-                // console.log(screenshotMap.keys());
             });
         }
     });
 }
 
 function compareDataURLs(oldURL, newURL, tabId) {
-    console.log("already contains a screenshot");
-    var diff = resemble(oldURL).compareTo(newURL).onComplete(function (data) {
-        console.log(data);
-        // console.log(data.getImageDataUrl());
-        var temp_color;
-        if (data.misMatchPercentage < 10) {
-            temp_color = "#2ecc71";
-        } else if (data.misMatchPercentage < 40) {
-            temp_color = "#f1c40f";
-        } else {
-            temp_color = "#e74c3c";
+    var oldImage = new Image();
+    var oldPieces = [];
+
+    oldImage.src = oldURL;
+    oldImage.onload = function () {
+        console.log("loading old image...")
+        for (var j = 0; j < oldImage.width; j += 10) {
+            for (var k = 0; k < oldImage.height; k += 10) {
+                var temp = document.createElement('canvas');
+                var context = temp.getContext('2d');
+                context.drawImage(oldImage, j, k, 10, 10, 0, 0, 10, 10);
+                oldPieces.push(temp.toDataURL());
+            }
         }
+        console.log("completed loading old image");
+    };
 
-        // original screenshots are used to set the color of the security indicator
-        // maybe change image color too (icon) - fixme
-        chrome.storage.sync.set({ color: temp_color }, null);
+    var newImage = new Image();
+    var newPieces = [];
 
-        var temp = new Image();
-        temp.src = data.getImageDataUrl();
-        temp.onload = function(){
-            var config = {oldURL: oldURL, newURL: newURL, imgHeight: temp.height, imgWidth: temp.width, color: temp_color};
-            chrome.tabs.executeScript(tabId, { code: 'var config = ' + JSON.stringify(config)}, function(){
-                console.log("Sending to tab...")
-                chrome.tabs.executeScript(tabId, {file: 'content.js'}, null);
+    newImage.src = newURL;
+    newImage.onload = function (){
+        console.log("loading new image...")
+        for (var j = 0; j < newImage.width; j += 10) {
+            for (var k = 0; k < newImage.height; k += 10) {
+                var temp = document.createElement('canvas');
+                var context = temp.getContext('2d');
+                context.drawImage(newImage, j, k, 10, 10, 0, 0, 10, 10);
+                newPieces.push(temp.toDataURL());
+            }
+        }
+        console.log("completed loading new image");
+
+        // yeet();
+        finishLoading();
+    };
+
+    function yeet() {
+        var diff_arr = [];
+        resemble.outputSettings({ useCrossOrigin: false });
+        for(var i = 0; i < 10; i++){
+            var diff = resemble(oldPieces[i]).compareTo(newPieces[i]).onComplete(function (data) {
+
+                console.log(data.misMatchPercentage);
+
+                if(data.misMatchPercentage > 0.1){
+                    diff_arr.push(1);
+                }else{
+                    diff_arr.push(0);
+                }
+
+                if (diff_arr.length >= 10){
+                    console.log(diff_arr);
+                    // sendData(diff_arr);
+                }
+
             });
         };
+    }
 
-        // now parse image in 10x10 pixels and map it onto an overlay?
-    });
+    function finishLoading() {
+        // go through all of the peices and do the resemble thing, id datamismatch greater than 50, matrix will have a 1, otherwise a 0
+        var diff_arr = [];
+
+        for (var i = 0; (i < oldPieces.length) && (i < newPieces.length); i++) {
+            var diff = resemble(oldPieces[i]).compareTo(newPieces[i]).onComplete(function (data) {
+
+                if(data.misMatchPercentage > 0.1){
+                    diff_arr.push(1);
+                } else {
+                    diff_arr.push(0);
+                }
+                console.log("Processing...", (diff_arr.length / oldPieces.length) * 100, '%');
+
+                if (diff_arr.length >= oldPieces.length) {
+                    console.log(diff_arr);
+                    sendData(diff_arr);
+                }
+
+            });
+
+        }
+    }
+
+    function sendData(diff_arr) {
+        var diff = resemble(oldURL).compareTo(newURL).onComplete(function (data) {
+            var temp_color;
+            if (data.misMatchPercentage < 10) {
+                temp_color = "#2ecc71";
+            } else if (data.misMatchPercentage < 40) {
+                temp_color = "#f1c40f";
+            } else {
+                temp_color = "#e74c3c";
+            }
+
+            chrome.storage.sync.set({ color: temp_color }, null);
+
+            var diffImage = new Image();
+            diffImage.src = data.getImageDataUrl();
+            diffImage.onload = function () {
+                var config = { diffArr: diff_arr, imgWidth: diffImage.width, imgHeight: diffImage.height };
+
+                chrome.tabs.executeScript(tabId, { code: 'var config = ' + JSON.stringify(config) }, function () {
+                    console.log("Sending to tab...")
+                    chrome.tabs.executeScript(tabId, { file: 'content.js' }, null);
+                });
+            }
+        });
+    }
 }
--- content.js
+++ content.js
@@ -1,23 +1,12 @@
-// now parse image in 10x10 pixels and map it onto an overlay
-
-// console.log("old:" + config.oldURL);
-// console.log("new:" + config.newURL);
-
-var oldURL = config.oldURL;
-var newURL = config.newURL;
-
-// console.log(oldURL);
-// console.log(newURL);
-var image1 = new Image();
-image1.src = config.oldURL;
-var image2 = new Image();
-image2.src = config.newURL;
+// there's a bug where when switching back and there's already an overlay,
+// it will take a screenshot with the overlay on top --> will consider it different than before
+//
+// remove overlay before taking screenshot
 
 console.log(config)
 
 var overlay = document.createElement("canvas");
 overlay.id = "overlay";
-overlay.style.backgroundColor = config.color;
 overlay.style.position = "fixed";
 overlay.style.top = "0";
 overlay.style.left = "0";
@@ -28,8 +17,30 @@ overlay.style.padding = "0";
 overlay.style.width = (config.imgWidth + "px");
 overlay.style.height = (config.imgHeight + "px");
 
+var context = overlay.getContext('2d');
+
+var i = 0;
+
+for (var j = 0; j < config.imgWidth; j+=10) {
+    for (var k = 0; k < config.imgHeight; k+=10) {
+        var bg;
+        if(config.diffArr[i] == 0){
+            bg = "#000000";
+        }else{
+            bg = "#e74c3c";
+        }
+
+        console.log(config.diffArr[i]);
+
+        context.fillStyle = bg;
+        context.fillRect(j, k, 10, 10);
+
+        i++;
+    }
+}
+
 var temp = document.getElementById("overlay");
-if((temp)){
+if ((temp)) {
     console.log("already here...")
     document.body.removeChild(temp);
 }
